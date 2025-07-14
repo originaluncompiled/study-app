8c6b0020491f543189abd81838c190662fb363a5
originaluncompiled <carstendold1@gmail.com>
2025-07-14 13:52:01 +0200

    reset project and added packages

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View className="flex-1 justify-center items-center bg-amber-50">
+            <Text className="font-bold">Stuff is supposed to go here.</Text>
+        </View>
+    );
+}
